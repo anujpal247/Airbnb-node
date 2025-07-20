9b8e9234a23f3dba590bec242be443da2eed838a
Anuj Pal <palanuj247@gmail.com>
2025-07-20 09:39:17 +0530

    Updated db config in hotel service

--- HotelService/src/config/db.config.js
+++ HotelService/src/config/db.config.js
@@ -9,7 +9,7 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "postgres",
+    dialect: "mysql",
     port: process.env.DB_PORT,
   },
 };
--- HotelService/src/config/index.ts
+++ HotelService/src/config/index.ts
@@ -3,13 +3,11 @@ import dotenv from "dotenv";
 
 type ServerConfig = {
   PORT: number;
-};
-
-type DatabaseConfig = {
-  PORT: number;
-  USERNAME: string;
-  PASSWORD: string;
-  HOST: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_NAME: string;
+  DB_HOST: string;
+  DB_PORT: number;
 };
 
 function loadEnv() {
@@ -21,11 +19,9 @@ loadEnv();
 
 export const serverConfig: ServerConfig = {
   PORT: Number(process.env.PORT) || 3001,
-};
-
-export const databaseConfig: DatabaseConfig = {
-  PORT: Number(process.env.DB_PORT) || 5433,
-  HOST: process.env.DB_HOST || "localhost",
-  USERNAME: process.env.DB_USER || "postgres",
-  PASSWORD: process.env.DB_PASSWORD || "root",
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_PORT: Number(process.env.DB_PORT) || 3306,
+  DB_PASSWORD: process.env.DB_PASSWORD || "root",
+  DB_NAME: process.env.DB_NAME || "dev",
+  DB_USER: process.env.DB_USER || "root",
 };
